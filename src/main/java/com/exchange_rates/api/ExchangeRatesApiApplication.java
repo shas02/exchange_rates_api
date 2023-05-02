a0b757da0f1900b189b802a078de9e3290b05fc9
Oleksandr Popov <shas02qazxsw@gmail.com>
2023-05-02 10:11:01 +0300

    initial commit

---
+++ src/main/java/com/exchange_rates/api/ExchangeRatesApiApplication.java
@@ -0,0 +1,13 @@
+package com.exchange_rates.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exchange_rates/api/ExchangeRatesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange_rates.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
